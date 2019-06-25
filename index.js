1934580d0f5e9b6bfbe5b4a640e8a10ce96451a0
Tim Lee <timothy.lee@intlfcstone.com>
2019-06-25 00:51:13 -0400

    Added server and index.html

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+const http = require('http').createServer(app);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public', 'index.html'));
+});
+
+http.listen(PORT, () => {
+    console.group(`Cosmic Messenger listening on port: ${PORT}`);
+});
