c384b21b83d3b284721ab680fb540ebd263a1e6c
Tim Lee <timothy.lee@intlfcstone.com>
2019-06-29 22:22:06 -0400

    Added another warning param to the mongoose.connect. Added bcrypt to salt/hash the plain text pw. Saved user (can't use same email) to db

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,31 +1,61 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const gravatar = require('gravatar');
+const { check, validationResult } = require('express-validator');
+const User = require('../../models/User');
+const bcrypt = require('bcryptjs');
 
 // @route   POST api/users
 // @desc    Test route 
 // @access  Public
 router.post('/', [
-    check(
-        'name',
-        'Name is required')
-        .not()
-        .isEmpty(),
-    check(
-        'email',
-        'Please include a valid email')
-        .isEmail(),
-    check(
-        'password',
-        'Please enter a password with 6 or more characters').isLength({ min: 5 })
-], (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(422).json({
-            errors: errors.array()
-        });
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({
+                errors: errors.array()
+            });
+        }
+
+        const { name, email, password } = req.body;
+
+        try {
+
+            // See if user exists
+            let user = await User.findOne({ email });
+            if (user) {
+                return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+            }
+
+            // Get users gravatar
+            const avatar = gravatar.url(email, {
+                s: '200',
+                r: 'pg',
+                d: 'mm'
+            });
+
+            user = new User({
+                name,
+                email,
+                avatar,
+                password
+            })
+
+            // Encrypt pw
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password, salt);
+            await user.save();
+
+            // Return JWT
+            res.send('User registered');
+        } catch (err) {
+            res.status(500).send('Server error - error adding user');
+        }
     }
-    res.send('User route');
-});
+);
 
 module.exports = router;
