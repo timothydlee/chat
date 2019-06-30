04c946c9d4bd29c20ce5219f860b197179365fb5
Tim Lee <timothy.lee@intlfcstone.com>
2019-06-30 16:42:12 -0400

    Added login route functionality.

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const User = require('../../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 
 // @route   GET api/auth
 // @desc    Test route 
@@ -12,8 +16,68 @@ router.get('/', auth, async (req, res) => {
         res.json(user);
     } catch (err) {
         console.err(err.message);
-        res.status(500).send('Server error');
+        res.status(500).send('Server Error');
     }
 });
+
+// @route   POST api/auth
+// @desc    Authenticate user and get token 
+// @access  Public
+router.post('/',
+    [
+        check('email', 'Please include a valid email.').isEmail(),
+        check('password', 'Password is required.').exists()
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({
+                errors: errors.array()
+            });
+        }
+
+        const { email, password } = req.body;
+
+        try {
+            // See if user exists
+            let user = await User.findOne({ email });
+
+            // If user exists, we return an error in the same format as the express-validator error format.
+            if (!user) {
+                return res
+                    .status(403)
+                    .json({ errors: [{ msg: 'Invalid credentials. Please try again.' }] });
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            if (!isMatch) {
+                console.log('Not matched');
+                return res
+                    .status(400)
+                    .json({ errors: [{ msg: 'Invalid credentials. Please try again.' }] })
+            }
+
+            // Return JWT
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
+
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                { expiresIn: 360000 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            );
+        } catch (err) {
+            res.status(500).send('Server Error - Something went wrong.');
+        }
+    }
+);
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -10,11 +10,12 @@ const config = require('config');
 // @route   POST api/users
 // @desc    Test route 
 // @access  Public
-router.post('/', [
-    check('name', 'Name is required').not().isEmpty(),
-    check('email', 'Please include a valid email').isEmail(),
-    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
-],
+router.post('/',
+    [
+        check('name', 'Name is required').not().isEmpty(),
+        check('email', 'Please include a valid email').isEmail(),
+        check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+    ],
     async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
