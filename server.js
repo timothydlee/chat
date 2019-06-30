39e37c44f938064e8b2f3f25901ae1fa84824de9
Tim Lee <timothy.lee@intlfcstone.com>
2019-06-29 20:55:35 -0400

    Added some routes

--- server.js
+++ server.js
@@ -8,3 +8,9 @@ connectDB();
 
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
 app.get('/', (req, res) => res.send(`API running`));
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
