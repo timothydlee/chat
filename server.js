01674beb929a2b4ffad2a4ca818be89c070dfafc
Tim Lee <timothy.lee@intlfcstone.com>
2019-06-29 22:04:05 -0400

    Added express-validator to the api routes.

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 5000;
 // Connect Database
 connectDB();
 
+// Initializing Middleware
+app.use(express.json({ extended: false }));
+
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
 app.get('/', (req, res) => res.send(`API running`));
 
