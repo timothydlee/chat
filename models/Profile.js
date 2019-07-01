fa102065be0119b32236c5b238ce1b2d9c05106b
Tim Lee <timothy.lee@intlfcstone.com>
2019-06-30 23:30:22 -0400

    Added route for getting logged in user's profile.

--- models/Profile.js
+++ models/Profile.js
@@ -67,7 +67,7 @@ const ProfileSchema = new mongoose.Schema({
                 required: true
             },
             from: {
-                type: Date
+                type: Date,
                 required: true
             },
             to: {
